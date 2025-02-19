4848486af65f749c8b2026904a72e44ff65228be
p34-pac <peterpaultolulope@gmail.com>
2025-02-19 05:13:16 +0100

    feat: add .env file for MongoDB connection string and update database access in get.py

--- app/database/get.py
+++ app/database/get.py
@@ -1,14 +1,16 @@
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 import os
-
+from dotenv import load_dotenv
 # Load environment variables from .env
 
 def get_database():
-    # Fetching the connection string from .env    
+    load_dotenv()
+    # Fetching the connection string from .env
+    envar = os.getenv("MongoUri") 
     
     # Connect to MongoDB Atlas (pass the URI directly)
-    client = MongoClient("mongodb+srv://quizeen:Asterixh@cluster0.8zxmg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")        
+    client = MongoClient(envar)        
     # Return the selected database
     return client.get_database("anikii")
     
